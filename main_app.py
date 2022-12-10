0f6b2a4277c0433c71ff7c93e5a494c640f99b18
mtszc <mtszccr@gmail.com>
2022-12-10 16:45:58 +0100

    Add converter class

--- main_app.py
+++ main_app.py
@@ -2,23 +2,49 @@ import customtkinter as ctk
 from tkinter import filedialog
 from pytube import Playlist
 from PIL import Image
+from moviepy.editor import VideoFileClip
+
+
+class Converter:
+    def __init__(self, video_file, audio_file):
+        self.video_file = video_file
+        self.audio_file = audio_file
+
+    def convert_mp4_to_mp3(self):
+        video_clip = VideoFileClip(self.video_file)
+        audio_clip = video_clip.audio
+        audio_clip.write_audiofile(self.audio_file)
+        video_clip.close()
+        audio_clip.close()
 
 
 class Downloader:
     def download_playlist(self):
         yt_playlist = Playlist(str(self.link.get()))
-        path = str(self.download_path.get())
         counter = 1
-        for video in yt_playlist.videos:
-            file_name = self.prepare_name_for_playlist(counter, video.title)
-            video.streams.get_highest_resolution().download(output_path=path, filename=file_name)
-            print("Downloaded: ", file_name)
-            counter = counter + 1
-        print("\nAll videos have been downloaded.")
+        if bool(self.is_audio_only.get()):
+            for video in yt_playlist.videos:
+                file_name = self.prepare_name_for_audio_playlist(counter, video.title)
+                video.streams.get_audio_only().download(output_path=str(self.download_path.get()), filename=file_name)
+                print("Downloaded: ", file_name)
+                counter = counter + 1
+            print("\nAll tracks have been downloaded.")
+        else:
+            for video in yt_playlist.videos:
+                file_name = self.prepare_name_for_video_playlist(counter, video.title)
+                video.streams.get_highest_resolution().download(
+                    output_path=str(self.download_path.get()), filename=file_name)
+                print("Downloaded: ", file_name)
+                counter = counter + 1
+            print("\nAll videos have been downloaded.")
 
     @staticmethod
-    def prepare_name_for_playlist(counter, name):
+    def prepare_name_for_video_playlist(counter, name):
         return "0" + str(counter) + " " + name + ".mp4" if counter < 10 else str(counter) + " " + name + ".mp4"
+
+    @staticmethod
+    def prepare_name_for_audio_playlist(counter, name):
+        return "0" + str(counter) + " " + name + ".mp3" if counter < 10 else str(counter) + " " + name + ".mp3"
 
     @staticmethod
     def file_system_explorer(event, download_path):
@@ -52,12 +78,12 @@ class Downloader:
                                          text="File format",
                                          font=("Arial", 32, "bold"))
         self.format_label.grid(row=3, columnspan=7)
-        self.format_switch = ctk.IntVar()
-        self.format_switch.set(0)
+        self.is_audio_only = ctk.IntVar()
+        self.is_audio_only.set(0)
         self.format_radiobutton_1 = ctk.CTkRadioButton(window, height=40, text="VIDEO", text_color="black",
-                                                       variable=self.format_switch, value=0)
+                                                       variable=self.is_audio_only, value=0)
         self.format_radiobutton_2 = ctk.CTkRadioButton(window, height=40, text="AUDIO", text_color="black",
-                                                       variable=self.format_switch, value=1)
+                                                       variable=self.is_audio_only, value=1)
         self.format_radiobutton_1.grid(row=4, columnspan=7)
         self.format_radiobutton_2.grid(row=5, columnspan=7)
         # TODO: QUANTITY CHOICE
@@ -66,7 +92,7 @@ class Downloader:
                                            font=("Arial", 32, "bold"))
         self.quantity_label.grid(row=6, columnspan=7)
         self.quantity_switch = ctk.IntVar()
-        self.format_switch.set(0)
+        self.quantity_switch.set(0)
         self.quantity_radiobutton_1 = ctk.CTkRadioButton(window, height=40, text="SINGLE", text_color="black",
                                                          variable=self.quantity_switch, value=0)
         self.quantity_radiobutton_2 = ctk.CTkRadioButton(window, height=40, text="PLAYLIST", text_color="black",
